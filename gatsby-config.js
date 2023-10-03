137ea0375357017f4e0af93b899eec7f16207d44
Stephane Nouvellon <stephane@cloudflare.com>
2023-10-03 11:00:33 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Feature request research`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
